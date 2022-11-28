47e6b4f2c529d4447789942f4d9f553c6b9ae384
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-28 13:51:31 +0300

    Done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -12,12 +12,12 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   #Basic Error Messaging when Rendering JSON Data 
   def show
-    cheese = Cheese.find_by(id: params[:id])
-    if cheese
-      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
-    else
-      # status: :not_found will produce a 404 status code
-      render json: { error: 'Cheese not found' }, status: :not_found
-    end
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    # status: :not_found will produce a 404 status code
+    render json: { error: 'Cheese not found' }, status: :not_found
   end
+end
 end
