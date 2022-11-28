45defc8279f5242bc62080179a299a525ac653cd
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-28 13:28:19 +0300

    Taking some custom built in serialization

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,33 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  #taking some custom built-in-serialization
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+  #Before Taking advantage of some custom built-in-serialization
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #   id: cheese.id,
+  #   name: cheese.name,
+  #   price: cheese.price,
+  #   is_best_seller: cheese.is_best_seller
+  # }
+  # end
+
+  
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #   id: cheese.id,
+  #   name: cheese.name,
+  #   price: cheese.price,
+  #   is_best_seller: cheese.is_best_seller
+  # }
+  # end
 
 end
