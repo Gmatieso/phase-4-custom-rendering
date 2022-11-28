b0c43713cfa2d3fb800a078f26fff63f52036d30
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-28 13:37:16 +0300

    Extending JSON Data with :methods

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,5 +1,7 @@
 class CheesesController < ApplicationController
-
+  #The Only and Expect options are available thanks to 
+  #the .as_json method, which Rails uses internally when
+  #we call render json with an Active Record object 
   # GET /cheeses
   #taking some custom built-in-serialization
   def index
@@ -8,26 +10,11 @@ class CheesesController < ApplicationController
   end
 
   # GET /cheeses/:id
-  #Before Taking advantage of some custom built-in-serialization
-  # def show
-  #   cheese = Cheese.find_by(id: params[:id])
-  #   render json: {
-  #   id: cheese.id,
-  #   name: cheese.name,
-  #   price: cheese.price,
-  #   is_best_seller: cheese.is_best_seller
-  # }
-  # end
+  # Before Taking advantage of some custom built-in-serialization
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  end
 
-  
-  # def show
-  #   cheese = Cheese.find_by(id: params[:id])
-  #   render json: {
-  #   id: cheese.id,
-  #   name: cheese.name,
-  #   price: cheese.price,
-  #   is_best_seller: cheese.is_best_seller
-  # }
-  # end
 
 end
