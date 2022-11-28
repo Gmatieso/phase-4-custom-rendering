c799ee491d505bdd492ae9d74f15c41ef2b1f46b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-28 13:48:54 +0300

    Adding a more descriptive response at the network

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -16,7 +16,8 @@ class CheesesController < ApplicationController
     if cheese
       render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
     else
-      render json: { error: 'Cheese not found' }
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
     end
   end
 end
